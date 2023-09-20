8834bca071fa0e147cc38ccaf0fca1d7a60b0b6f
Pasit DE <ezynook@gmail.com>
2023-09-20 16:25:02 +0700

    ...

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -Eeo pipefail
 
-Gre='\e[0;32m'
-Red='\e[0;31m'
-Whi='\e[0;37m'
-
 echo "Linux Cleanup Installation...."
 curl -o linux-cleanup https://raw.githubusercontent.com/ezynook/linux-cleanup/main/linux-cleanup
 mv linux-cleanup /usr/local/bin/linux-cleanup
 chmod +x /usr/local/bin/linux-cleanup
-echo -e "${Gre} Install Linux-Cleanup has Successfully ${Whi}"...
+echo -e "Install Linux-Cleanup has Successfully"
 sleep 3
 exit 0
