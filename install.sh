226cd191ea2f1c3468f34f8360eb07f8bc441a5d
Pasit DE <ezynook@gmail.com>
2023-09-20 16:25:40 +0700

    fixed err code..

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ echo "Linux Cleanup Installation...."
 curl -o linux-cleanup https://raw.githubusercontent.com/ezynook/linux-cleanup/main/linux-cleanup
 mv linux-cleanup /usr/local/bin/linux-cleanup
 chmod +x /usr/local/bin/linux-cleanup
-echo -e "Install Linux-Cleanup has Successfully"
+echo "Install Linux-Cleanup has Successfully"
 sleep 3
 exit 0
