f499888a4975b68b509acee0bb12e3854a62ba90
Pasit DE <ezynook@gmail.com>
2023-09-20 16:20:30 +0700

    replace script to news

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ Red='\e[0;31m'
 Whi='\e[0;37m'
 
 echo "Linux Cleanup Installation...."
-curl -o linux-cleanup https://raw.githubusercontent.com/ezynook/linux-cleanup/main/log_purge
+curl -o linux-cleanup https://raw.githubusercontent.com/ezynook/linux-cleanup/main/linux-cleanup
 mv linux-cleanup /usr/local/bin/linux-cleanup
 chmod +x /usr/local/bin/linux-cleanup
 echo "${Gre}Install Linux-Cleanup has Successfully${Whi}"...
