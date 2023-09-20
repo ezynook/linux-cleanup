e805853a90cd18c554f030bdf7355061c16f136c
Pasit DE <ezynook@gmail.com>
2023-09-20 16:21:44 +0700

    bugs script resolved

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ echo "Linux Cleanup Installation...."
 curl -o linux-cleanup https://raw.githubusercontent.com/ezynook/linux-cleanup/main/linux-cleanup
 mv linux-cleanup /usr/local/bin/linux-cleanup
 chmod +x /usr/local/bin/linux-cleanup
-echo "${Gre}Install Linux-Cleanup has Successfully${Whi}"...
+echo -e "${Gre}Install Linux-Cleanup has Successfully${Whi}"...
 sleep 3
 exit 0
