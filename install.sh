ca9bc08865d79fb68d92632ca9c73b86a7d2ac50
Pasit DE <ezynook@gmail.com>
2023-09-21 10:04:30 +0700

    add uninstall script

--- install.sh
+++ install.sh
@@ -2,13 +2,23 @@
 
 set -o pipefail
 
-Red='\e[0;31m'
+Gre='\e[0;32m'
 Whi='\e[0;37m'
 
-echo -e "${Red}Linux Cleanup Installation....${Whi}"
-curl -o linux-cleanup https://raw.githubusercontent.com/ezynook/linux-cleanup/main/src/linux-cleanup
-mv linux-cleanup /usr/local/bin/linux-cleanup
-chmod +x /usr/local/bin/linux-cleanup
-echo -e "${Red}Install Linux-Cleanup has Successfully!${Whi}"
-sleep 3
-exit 0
+if [ "$1" == "--install" ]; then
+    echo -e "${Gre}Linux Cleanup Installation....${Whi}"
+    curl -o linux-cleanup https://raw.githubusercontent.com/ezynook/linux-cleanup/main/src/linux-cleanup
+    mv linux-cleanup /usr/local/bin/linux-cleanup
+    chmod +x /usr/local/bin/linux-cleanup
+    echo -e "${Gre}Install Linux-Cleanup has Successfully!${Whi}"
+    sleep 3
+    exit 0
+elif [ "$1" == "--uninstall" ]; then
+    rm -f /usr/local/bin/linux-cleanup
+    echo -e "${Gre}Uninstall Linux-Cleanup has Successfully!${Whi}"
+else
+    echo "Running Script Parameter"
+    echo "--install         =       Installing Linux-Cleanup"
+    echo "--uninstall       =       Uninstall Linux-Cleanup"
+    exit 1
+fi
