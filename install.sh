eac34d42e75e8ade63b607fef41f98a5fff371ec
Pasit DE <ezynook@gmail.com>
2023-09-21 09:31:11 +0700

    fixed bugs

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-set -Eeo pipefail
+
+set -o pipefail
+
+Red='\e[0;31m'
+Whi='\e[0;37m'
 
 echo "Linux Cleanup Installation...."
 curl -o linux-cleanup https://raw.githubusercontent.com/ezynook/linux-cleanup/main/linux-cleanup
 mv linux-cleanup /usr/local/bin/linux-cleanup
 chmod +x /usr/local/bin/linux-cleanup
-echo "Install Linux-Cleanup has Successfully"
+echo -e "${Gre}Install Linux-Cleanup has Successfully!${Whi}"
 sleep 3
 exit 0
