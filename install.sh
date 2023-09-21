410f2b5e5f3f4a47b291a023b3889883001266c5
Pasit DE <ezynook@gmail.com>
2023-09-21 09:45:14 +0700

    move script path

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ set -o pipefail
 Red='\e[0;31m'
 Whi='\e[0;37m'
 
-echo "Linux Cleanup Installation...."
+echo -e "${Red}Linux Cleanup Installation....${Whi}"
 curl -o linux-cleanup https://raw.githubusercontent.com/ezynook/linux-cleanup/main/linux-cleanup
 mv linux-cleanup /usr/local/bin/linux-cleanup
 chmod +x /usr/local/bin/linux-cleanup
-echo -e "${Gre}Install Linux-Cleanup has Successfully!${Whi}"
+echo -e "${Red}Install Linux-Cleanup has Successfully!${Whi}"
 sleep 3
 exit 0
